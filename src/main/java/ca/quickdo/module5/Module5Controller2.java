78939795bb269d767442fff5481d8d7f245bff67
Conrad Bekondo <bekondo@quickdo.fr>
2024-04-11 10:30:05 +0100

    chore: Delete button

--- src/main/java/ca/quickdo/module5/Module5Controller2.java
+++ src/main/java/ca/quickdo/module5/Module5Controller2.java
@@ -4,6 +4,7 @@ import ca.quickdo.module5.model.Person;
 import ca.quickdo.module5.services.PeopleService;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
@@ -53,7 +54,7 @@ public class Module5Controller2 {
     private TableView<Person> tvPeople;
 
     @FXML
-    private TableColumn<Person, Node> colAvatar;
+    private TableColumn<Person, String> colAvatar;
 
     @FXML
     private TableColumn<Person, String> colName;
@@ -68,36 +69,63 @@ public class Module5Controller2 {
     private TableColumn<Person, String> colCity;
 
     @FXML
-    private TableColumn<Person, Object> colAction;
+    private TableColumn<Person, Integer> colAction;
+
+    private void deletePerson(Person person) {
+        executorService.submit(() -> {
+            try {
+                PeopleService.deleteById(person.getId());
+                Platform.runLater(() -> people.remove(person));
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        });
+    }
 
     @FXML
     private void initialize() {
-        colName.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getNames()));
-        colPhone.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getPhone()));
-        colEmail.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getEmail()));
-        colCity.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getCity()));
-        colAvatar.setCellFactory(param -> new TableCell<Person, Node>() {
+        colAction.setSortable(false);
+        colAction.setCellFactory(param -> new TableCell<>() {
             @Override
-            protected void updateItem(Node node, boolean isEmpty) {
-                super.updateItem(node, isEmpty);
-                if (!isEmpty) {
+            protected void updateItem(Integer o, boolean empty) {
+                super.updateItem(o, empty);
+                if (!empty) {
+                    final var node = new Button("Delete");
+
+                    node.setOnAction(e -> deletePerson(getTableRow().getItem()));
                     setGraphic(node);
                 } else {
                     setGraphic(null);
                 }
             }
         });
-        colAvatar.setCellValueFactory(param -> {
-            ImageView imageView = new ImageView();
-            executorService.submit(() -> {
-                try (final var imageStream = new URL(param.getValue().getAvatar()).openConnection().getInputStream()) {
-                    Platform.runLater(() -> imageView.setImage(new Image(imageStream)));
-                } catch (IOException ex) {
-                    System.err.println(ex);
+        colName.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getNames()));
+        colPhone.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getPhone()));
+        colEmail.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getEmail()));
+        colCity.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getCity()));
+        colAvatar.setCellFactory(param -> new TableCell<Person, String>() {
+            @Override
+            protected void updateItem(String node, boolean isEmpty) {
+                super.updateItem(node, isEmpty);
+                if (!isEmpty && node != null && !node.isEmpty()) {
+                    final var iv = new ImageView();
+                    executorService.submit(() -> {
+                        try (var stream = new URL(node).openConnection().getInputStream()) {
+                            Platform.runLater(() -> {
+                                final var image = new Image(stream);
+                                iv.setImage(image);
+                            });
+                        } catch (Exception ex) {
+                            System.err.println(ex);
+                        }
+                    });
+                    setGraphic(iv);
+                } else {
+                    setGraphic(null);
                 }
-            });
-            return new SimpleObjectProperty<>(imageView);
+            }
         });
+        colAvatar.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getAvatar()));
         tvPeople.setItems(people);
         cbCity.setItems(cities);
         executorService.submit(() -> {
